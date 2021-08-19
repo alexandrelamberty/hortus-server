32556a72ef83fde6601d43035b87386b7b2201af
Alexandre Lamberty <alexandrelamberty@gmail.com>
2021-08-19 19:19:16 +0200

    Config update and package.json

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,6 +4,7 @@ export default () => ({
   mongo: {
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
+    name: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     uri: process.env.DATABASE_URI,
