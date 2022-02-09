ee0c92c97b3defb695eec720be91679a6b726e3b
Alexandre Lamberty <alexandrelamberty@gmail.com>
2022-02-09 11:51:37 +0100

    Add logger and global filters

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,35 @@ import {
   ValidationPipe,
   VersioningType,
 } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
+import { HttpAdapterHost, NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
+import { BadRequestExceptionFilter } from './common/exceptions/bad-request.exception.filter'
+import { ExceptionsLoggerFilter } from './common/exceptions/exceptionLogger.filter'
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter'
+import { MongoExceptionFilter } from './common/exceptions/mongo-exception.filters'
 
 async function bootstrap() {
   const logger = new Logger('Main')
 
   // Check types or interfaces; NestExpressApplication
-  const app = await NestFactory.create<INestApplication>(AppModule, {})
+  const app = await NestFactory.create<INestApplication>(AppModule, {
+		logger: logger
+	})
+
+  // Exception Filters - https://docs.nestjs.com/exception-filters
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new ExceptionsLoggerFilter(httpAdapter),
+    new BadRequestExceptionFilter(),
+    new MongoExceptionFilter()
+  )
+
+  // Pipes - https://docs.nestjs.com/pipes
   app.useGlobalPipes(new ValidationPipe())
 
-  app.useGlobalPipes(new ValidationPipe())
   // Versioning - https://docs.nestjs.com/techniques/versioning#versioning
   app.enableVersioning({
     type: VersioningType.URI,
